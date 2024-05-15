4bdc0f723f8ace6bb51bf4f562df839ce21d5fc7
Shivam <shivamk.services@gmail.com>
2024-05-15 15:03:20 +0530

    just some changes

--- app.js
+++ app.js
@@ -5,35 +5,17 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   webVersionCache: {
     type: "remote",
-    remotePath:
-      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+    remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
   },
 });
 
 client.initialize();
-
-client.on('loading_screen', (percent, message) => {
-  console.log('LOADING SCREEN', percent, message);
-});
-
-client.on("qr", (qr) => {
-  qrcode.generate(qr, { small: true });
-});
-
-client.on("authenticated", () => {
-  console.log("AUTHENTICATED");
-});
-
-client.on('auth_failure', msg => {
-  console.error('AUTHENTICATION FAILURE', msg);
-});
-
-client.on("ready", () => {
-  console.log("Client is ready!");
-});
+client.on("qr", (qr) => qrcode.generate(qr, { small: true }));
+client.on("authenticated", () => console.log("AUTHENTICATED"));
+client.on('auth_failure', msg => console.error('AUTHENTICATION FAILURE', msg));
+client.on("ready", () => console.log("Client is ready!"));
 
 client.on("message", (message) => {
-  console.log(message);
-  const replyMessage = `Hi ${message._data.notifyName.split(" ")[0]}!, This is an auto-generated message. I am currently not available. I will get back to you soon. Thanks!`;
-  client.sendMessage(message.from, replyMessage);
+  const nameOfSender = message._data.notifyName.split(" ")[0];
+  client.sendMessage(message.from, `Hi ${nameOfSender}!, This is an auto-generated message. I am currently not available. I will get back to you soon. Thanks!`);
 });
